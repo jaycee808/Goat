d6b4532a21d482ceaad0659c8ae84d587cad62fd
jaycee808 <jamie.clark@live.co.uk>
2023-06-30 15:53:04 +0100

    updated shopping cart toggle and menu-link toogle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menuLinks = document.querySelector('.menu-links');
-    
+    const menuCloseBtn = document.querySelector('.menu-close-btn');
+    const cartToggle = document.querySelector('.toggle-cart');
+    const cartOverlay = document.querySelector('.cart-overlay');
+
     menuToggle.addEventListener('click', () => {
         menuLinks.classList.toggle('show');
+        menuLinks.classList.toggle('grid');
+        menuCloseBtn.style.display = 'block'; // Display the close button when menu links are shown
+    });
+
+    menuCloseBtn.addEventListener('click', () => {
+        menuLinks.classList.remove('show');
+        menuCloseBtn.style.display = 'none'; // Hide the close button when menu links are hidden
+    });
+
+    cartToggle.addEventListener('click', () => {
+        cartOverlay.classList.toggle('show');
+    });
+
+    const cartCloseBtn = document.querySelector('.cart-close-btn');
+    cartCloseBtn.addEventListener('click', () => {
+        cartOverlay.classList.remove('show');
     });
 });
