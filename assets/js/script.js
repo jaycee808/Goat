e1e68fe113a7b143390630d4e900a70c20a73e6b
jaycee808 <jamie.clark@live.co.uk>
2023-07-02 18:05:15 +0100

    updated products array and added display products function

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,142 +28,130 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // product lists //
-
-let cleansers = [
+let productList = [
     {
-        id: "1",
+        id: "c1",
         name: "cleanser one",
         price: "12",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At elementum eu facilisis sed odio morbi quis commodo odio. Gravida in fermentum et sollicitudin ac. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Tempor nec feugiat nisl pretium fusce.",
     },
     {
-        id: "2",
+        id: "c2",
         name: "cleanser two",
         price: "15",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Placerat vestibulum lectus mauris ultrices. Fermentum leo vel orci porta non pulvinar neque. ",
     },
     {
-        id: "3",
+        id: "c3",
         name: "cleanser three",
         price: "18",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet luctus venenatis lectus magna fringilla urna porttitor. Dui accumsan sit amet nulla facilisi morbi tempus. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Pulvinar neque laoreet suspendisse interdum consectetur libero id. ",
     },
-];
-
-let exfoliator = [
     {
-        id: "1",
+        id: "e1",
         name: "exfoliator one",
         price: "10",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "2",
+        id: "e2",
         name: "exfoliator two",
         price: "12",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "3",
+        id: "e3",
         name: "exfoliator three",
         price: "14",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
-];
-
-let moisturisers = [
     {
-        id: "1",
+        id: "m1",
         name: "moisturiser one",
         price: "20",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "2",
+        id: "m2",
         name: "moisturiser two",
         price: "25",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "3",
+        id: "m3",
         name: "moisturiser three",
         price: "30",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
-];
-
-let beardOil = [
     {
-        id: "1",
+        id: "b1",
         name: "beard oil one",
         price: "12",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "2",
+        id: "b2",
         name: "beard oil two",
         price: "10",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "3",
+        id: "b3",
         name: "beard oil three",
         price: "14",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
-];
-
-let colognes = [
     {
-        id: "1",
+        id: "f1",
         name: "cologne one",
         price: "50",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "2",
+        id: "f2",
         name: "cologne two",
         price: "55",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "3",
+        id: "f3",
         name: "cologne three",
         price: "50",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "4",
+        id: "f4",
         name: "cologne four",
         price: "55",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
-        id: "5",
+        id: "f5",
         name: "cologne five",
         price: "60",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
 ];
 
-// Function to display cleanser properties dynamically
-function displayCleanserProperties() {
-    const cleanserPropertiesDiv = document.getElementById('cleanser-properties');
+// Function to display products dynamically
+function displayProducts() {
+    const productDisplayDiv = document.getElementById('product-display');
 
-    let cleanserHTML = '';
-        for (let i = 0; i < cleansers.length; i++) {
-        const cleanser = cleansers[i];
+    let productHTML = '';
+        for (let i = 0; i < productList.length; i++) {
+        const product = productList[i];
 
-        cleanserHTML += `
+        productHTML += `
             <div>
-                <h3>${cleanser.name}</h3>
-                <p>Price: £${cleanser.price}</p>
-                <p>Description: ${cleanser.description}</p>
+                <h3>${product.name}</h3>
+                <p>Price: £${product.price}</p>
+                <p>Description: ${product.description}</p>
+                <button class="addToBasketBtn" data-id="${product.id}">Add to Basket</button>
             </div>
             `;
         }
 
-        cleanserPropertiesDiv.innerHTML = cleanserHTML;
+        productDisplayDiv.innerHTML = productHTML;
     }
 
-    displayCleanserProperties();
+displayProducts();
