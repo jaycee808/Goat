8e8938dfa68484d7b7de314186d004f9286c2ae8
jaycee808 <jamie.clark@live.co.uk>
2023-07-02 21:35:09 +0100

    updated js to work with new navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const cartToggle = document.querySelector('.toggle-cart');
     const cartOverlay = document.querySelector('.cart-overlay');
 
-    menuToggle.addEventListener('click', () => {
-        menuLinks.classList.toggle('show');
-        menuLinks.classList.toggle('grid');
-        menuCloseBtn.style.display = 'block'; // Display the close button when menu links are shown
-    });
-
-    menuCloseBtn.addEventListener('click', () => {
-        menuLinks.classList.remove('show');
-        menuCloseBtn.style.display = 'none'; // Hide the close button when menu links are hidden
-    });
-
     cartToggle.addEventListener('click', () => {
         cartOverlay.classList.toggle('show');
     });
 
     const cartCloseBtn = document.querySelector('.cart-close-btn');
-    cartCloseBtn.addEventListener('click', () => {
+        cartCloseBtn.addEventListener('click', () => {
         cartOverlay.classList.remove('show');
     });
 });
