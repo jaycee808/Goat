3d2ea82a4338fde46803e2d4557a5de6bba61c0e
jaycee808 <jamie.clark@live.co.uk>
2023-07-08 16:05:10 +0100

    updated product names

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,19 +23,19 @@ let productList = [
         products: [
             {
             id: "c1",
-            name: "cleanser one",
+            name: "wildwood",
             price: "12",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At elementum eu facilisis sed odio morbi quis commodo odio. Gravida in fermentum et sollicitudin ac. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Tempor nec feugiat nisl pretium fusce.",
             },
             {
             id: "c2",
-            name: "cleanser two",
+            name: "forest dew",
             price: "15",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Placerat vestibulum lectus mauris ultrices. Fermentum leo vel orci porta non pulvinar neque. ",
             },
             {
             id: "c3",
-            name: "cleanser three",
+            name: "oasis",
             price: "18",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet luctus venenatis lectus magna fringilla urna porttitor. Dui accumsan sit amet nulla facilisi morbi tempus. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Pulvinar neque laoreet suspendisse interdum consectetur libero id. ",
             },
@@ -46,19 +46,19 @@ let productList = [
         products: [
             {
             id: "e1",
-            name: "exfoliator one",
+            name: "oakwood",
             price: "10",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "e2",
-            name: "exfoliator two",
+            name: "cedar bark",
             price: "12",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "e3",
-            name: "exfoliator three",
+            name: "wildflowers",
             price: "14",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
@@ -69,19 +69,19 @@ let productList = [
         products: [
             {
             id: "m1",
-            name: "moisturiser one",
+            name: "golden oak",
             price: "20",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "m2",
-            name: "moisturiser two",
+            name: "amber nectar",
             price: "25",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "m3",
-            name: "moisturiser three",
+            name: "aqua luxe",
             price: "30",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
@@ -92,31 +92,37 @@ let productList = [
         products: [
             {
             id: "f1",
-            name: "cologne one",
+            name: "amber oak",
             price: "50",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "f2",
-            name: "cologne two",
+            name: "vanilla luxe",
             price: "55",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "f3",
-            name: "cologne three",
+            name: "wildfire",
             price: "50",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "f4",
-            name: "cologne four",
+            name: "redwood dusk",
             price: "55",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
             {
             id: "f5",
-            name: "cologne five",
+            name: "forest noir",
+            price: "60",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            },
+            {
+            id: "f6",
+            name: "midnight velvet",
             price: "60",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             },
@@ -223,7 +229,7 @@ function displayShoppingCart() {
         cartItemsHTML += `
             <article class="cart-item" data-id="${item.id}">
                 <div>
-                    <h4 class="cart-item-name">${item.name}</h4>
+                    <h4 class="cart-item-name">${item.name.toUpperCase()}</h4>
                     <p class="cart-item-price">£${item.price}</p>
                     <button class="cart-item-remove-btn">Remove</button>
                 </div>
